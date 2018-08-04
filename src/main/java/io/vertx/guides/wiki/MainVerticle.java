945a617829441abb6c1cc6ccf181fb040355aa17
stakah <stakah@gmail.com>
2018-08-03 23:07:28 -0300

    Separate HttpServerVerticle and WikiDatabaseVeticle

---
+++ src/main/java/io/vertx/guides/wiki/MainVerticle.java
@@ -0,0 +1,29 @@
+package io.vertx.guides.wiki;
+
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+
+
+public class MainVerticle extends AbstractVerticle {
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    Future<String> dbVerticleDeployment = Future.future();
+    vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment.completer());
+    dbVerticleDeployment.compose(id -> {
+      Future<String> httpVerticleDeployment = Future.future();
+      vertx.deployVerticle(
+        "io.vertx.guides.wiki.HttpServerVerticle",
+      new DeploymentOptions().setInstances(2),
+      httpVerticleDeployment.completer());
+      return httpVerticleDeployment;
+    }).setHandler(ar -> {
+      if (ar.succeeded()) {
+        startFuture.complete();
+      } else {
+        startFuture.fail(ar.cause());
+      }
+    });
+  }
+}
